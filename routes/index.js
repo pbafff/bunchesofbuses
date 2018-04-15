6d333d81851e63a7d65d40125eb2a6dfd8e8837d
Andre Myers <andre.myers99@gmail.com>
2018-04-15 03:16:34 -0400

    moved map zooming example to own route

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'B8 the dumb bus' });
 });
 
+router.get('/map-zooming', function(req, res, next) {
+  res.render('map-zooming', {title: 'Map Zooming example'});
+});
+
 router.get('/us-states.json', function(req, res, next) {
   res.json(usStates)
 });

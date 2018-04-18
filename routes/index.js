55a282512082d9cb22da32004f609cac05ad8d91
Andre Myers <andre.myers99@gmail.com>
2018-04-18 15:50:41 -0400

    added socket.io and BayRidgeModels.count to index.html

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var usStates = require('../us-states');
+var BayRidgeModels = require('../modules/bayridge');
+var precount;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'B8 the dumb bus' });
+router.get('/', function (req, res, next) {
+  BayRidgeModels.count({}, function (err, count) {
+    if (err) return handleError(err);
+    precount = count;
+    console.log(precount)
+  });
+  res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.render('index', { title: 'B8 the dumb bus', bayridgenumber: precount });
+  
 });
 
-router.get('/map-zooming', function(req, res, next) {
-  res.render('map-zooming', {title: 'Map Zooming example'});
+router.get('/map-zooming', function (req, res, next) {
+  res.render('map-zooming', { title: 'Map Zooming example' });
 });
 
-router.get('/us-states.json', function(req, res, next) {
+router.get('/us-states.json', function (req, res, next) {
   res.json(usStates)
 });
 

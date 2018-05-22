d6723ce938ce13c36bff07d51aca5b26e8e9e90b
pbafff <andre.myers99@gmail.com>
2018-05-22 14:25:33 -0400

    create React app

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//           hello world test
+//         </p>
+//       </div>
+//     )
+//   }
+// }
+class App extends Component {
+  state = {
+    response: []
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/index');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <h1>Trips</h1>
+
+        {/* <div className="App-intro">{this.state.response}</div> */}
+        {this.state.response.map(user =>
+          <div key={user.vehicleref}>{user.begin}</div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default App;

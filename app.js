140bf08b5f17264cc1462dfb45592a9ddb8fc8e2
Andre Myers <andre.myers99@gmail.com>
2018-05-24 23:18:35 -0400

    learning react

--- app.js
+++ app.js
@@ -39,6 +39,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve static files from the React app
+// app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use('/', index);
 app.use('/', apicall);
 

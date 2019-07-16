406586129ffbfc958d04868ecc0214eb456e80c0
Andre <andre.myers99@gmail.com>
2019-07-15 23:44:09 -0400

    changed database connection

--- db/index.js
+++ db/index.js
@@ -1,10 +1,9 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'power_user',
-    host: 'ec2-13-59-152-248.us-east-2.compute.amazonaws.com',
-    database: 'trips',
-    password: 'poweruserpassword',
+    user: 'postgres',
+    host: 'localhost',
+    database: 'buses',
     port: 5432,
 });
 
